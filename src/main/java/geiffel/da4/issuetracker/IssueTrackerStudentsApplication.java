fde0578587e43c6aaaced6f1a079cb70d3fa2d29
Jonathan LONGEVILLE <jlongeville@geiffel.fr>
2023-11-16 10:49:36 +0100

    fini les code smells

--- src/main/java/geiffel/da4/issuetracker/IssueTrackerStudentsApplication.java
+++ src/main/java/geiffel/da4/issuetracker/IssueTrackerStudentsApplication.java
@@ -30,6 +30,10 @@ public class IssueTrackerStudentsApplication {
     private final static User userMachin = new User(1L, "Machin", Fonction.USER);
     private final static User userChose = new User(2L, "Chose", Fonction.USER);
 
+    private final static Issue issue1 = new Issue(1L, "1st comm", "C'est ma 1re issue", userMachin);
+    private final static Issue issue2 = new Issue(2L, "2nd comm", "C'est ma 2nde issue", userMachin);
+    private final static Issue issue3 = new Issue(3L, "3e comm", "C'est ma 3re issue", new User(3L, "Truc", Fonction.DEVELOPPER));
+
     public static void main(String[] args) {
         SpringApplication.run(IssueTrackerStudentsApplication.class, args);
     }
@@ -44,15 +48,15 @@ public class IssueTrackerStudentsApplication {
 
 
             List<Issue> issues = new ArrayList<>();
-            issues.add(new Issue(1L, "1st comm", "C'est ma 1re issue", userMachin));
-            issues.add(new Issue(2L, "2nd comm", "C'est ma 2nde issue", userMachin));
-            issues.add(new Issue(3L, "3e comm", "C'est ma 1re issue", new User(3L, "Truc", Fonction.DEVELOPPER)));
+            issues.add(issue1);
+            issues.add(issue2);
+            issues.add(issue3);
 
 
             List<Commentaire> commentaires = new ArrayList<>();
             commentaires.add(new Commentaire(1L, new User(3L, "Truc", Fonction.DEVELOPPER), new Issue(2L, "2nd comm", "C'est ma 2nde issue", userMachin), "1er commentaire"));
-            commentaires.add(new Commentaire(2L, userChose, (new Issue(3L, "3e comm", "C'est ma 1re issue", new User(3L, "Truc", Fonction.DEVELOPPER))), "1er commentaire"));
-            commentaires.add(new Commentaire(3L, userChose, new Issue(2L, "2nd comm", "C'est ma 2nde issue", userMachin), "2nd commentaire"));
+            commentaires.add(new Commentaire(2L, userChose, issue3, "1er commentaire"));
+            commentaires.add(new Commentaire(3L, userChose, issue2, "2nd commentaire"));
 
             userRepository.saveAll(users);
             issueRepository.saveAll(issues);
